f7c3b5a6369b3edb49a9dd249e3f7f078933a969
JarlErikMathiesen <117684143+JarlErikMathiesen@users.noreply.github.com>
2023-11-04 18:36:26 +0100

    Removed console.logs, revised error-statements

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,22 @@
-
-
-const currentUrl = window.location.href;
-console.log(currentUrl);
-
-
 const url ="https://api.noroff.dev/api/v1/rainy-days";
-const corsEnabledUrl = "https://noroffcors.onrender.com/" + url;
-
 const jacketSale = document.querySelector(".index_jackets_box");
+const currency = "kr";
 
 async function jacketList() {
 
     try {
-        const response = await fetch(corsEnabledUrl);
+        const response = await fetch(url);
         const json = await response.json();
-
-        console.log(json);
-
-
         const jackets = json;
-        jacketSale.innerHTML =`     <h2>
-                                        Get ready for the rain season with our new arrivals
-                                    </h2>`
+
+        if (!response.ok) {
+            throw Error();
+        }
+
+        jacketSale.innerHTML =`
+                                <h2>
+                                    Get ready for the rain season with our sale!
+                                </h2>`
 
         for (let i = 0; i < jackets.length; i++) {
 
@@ -29,36 +24,34 @@ async function jacketList() {
                 continue;
             }
 
-            if (i >= 6){
+            if (i === 6){
                 break;
             } 
-
-
-            console.log(`jacket_page_${jackets[i].id}.html`);
-            console.log(jackets[i].gender);
 
           const jacketTitle = jackets[i].title;
           const jacketsImage = jackets[i].image;
           const jacketsPrice = jackets[i].price;
           const jacketsId = jackets[i].id;
           const jacketsDiscount = jackets[i].discountedPrice;
-          const jacketsGender = jackets[i].gender;
 
             
           jacketSale.innerHTML += `<div>                    
-                                            <a href="jacket_page_${jacketsId}.html" class="cardlink list_of_jackets_card"> 
-                                                <img class="list_of_jackets_img" src=${jacketsImage}>
-                                                <h2 class="cardhead">
-                                                    ${jacketTitle}
-                                                </h2>
-                                                <p class="list_of_jackets_price-tag">
-                                                    ${jacketsDiscount}
-                                                </p>
-                                            </a>
-                                        </div>`;
+                                        <a href="jacket_page.html?id=${jacketsId}" class="cardlink list_of_jackets_card"> 
+                                            <img class="index_jackets" src=${jacketsImage}>
+                                            <h2 class="cardhead">
+                                                ${jacketTitle}
+                                            </h2>
+                                            <p class="list_of_jackets_price-tag-old">
+                                                ${jacketsPrice} ${currency}
+                                            </p>
+                                            <p class="list_of_jackets_price-tag">
+                                                ${jacketsDiscount} ${currency}
+                                            </p>
+                                        </a>
+                                    </div>`;
         }
     }   catch(error){
-        jacketContainer.innerHTML = `<h2 class="error">Error</h2>`;
+        jacketSale.innerHTML =   `<div class="error">An error occured loading the page</div>`;
     }     
 }
 
--- JS/jacketDetail.js
+++ JS/jacketDetail.js
@@ -1,37 +1,22 @@
-const url = "https://api.noroff.dev/api/v1/rainy-days";
+const url = "https://api.noroff.dev/api/v1/rainy-days/";
 const jacketContainer = document.getElementById("specific_jacket");
+const urlPar = new URLSearchParams(document.location.search);
+const id = urlPar.get("id");
+const currency = "kr";
 
+const urlSingle = url + id;
 
-
-
-
-/* const jacketContainer = document.querySelector(".jacket_page_prime"); */
-
-async function getSpecificJacketIdFromQuery() {
-    const urlParams = new URLSearchParams(window.location.search);
-    console.log(urlParams);
-    return urlParams.get("id");
-  }
 
 async function jacketSingle() {
 
     try {
-      const jacketsId = await getSpecificJacketIdFromQuery();
-      if (!jacketsId) {
-        throw new Error("Jacket ID not found in the query parameters.");
-      }
 
-      const response = await fetch(`${url}/${jacketsId}`);
-  
+      const response = await fetch(urlSingle);
+
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-  
+        throw new Error();
+    }
       const jackets = await response.json();
-
-
-
-        console.log(jackets);  
 
         jacketContainer.innerHTML = `
                                     <div class="jacket_page_prime">
@@ -51,7 +36,7 @@ async function jacketSingle() {
                                     <p class="jacket_page_description">
                                       ${jackets.description}
                                     </p>
-                                    <p class="jacket_price">${jackets.price}</p>
+                                    <p class="jacket_price">${jackets.price} ${currency}</p>
                                     <div class="jacket_page_addbasket">
                                       <div class="blue-button add-to-basket-button">
                                         <a href="checkout_page.html"> Add to basket </a>
@@ -65,14 +50,7 @@ async function jacketSingle() {
                                     </div>
                                   `
     }   catch (error) {
-        console.error("An error occurred:", error);
-    
-        const errorMessage = document.createElement('div');
-        errorMessage.textContent = "An error occurred. Please try again later.";
-        errorMessage.classList.add('error-message');
-      
-        resultsContainer.innerHTML = "";
-        resultsContainer.appendChild(errorMessage);
+        jacketContainer.innerHTML =   `<div class="error">An error occured loading the page</div>`;
     }    
 }
 
